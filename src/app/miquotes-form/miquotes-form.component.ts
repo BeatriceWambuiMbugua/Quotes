6fcc0c2eef48f25118e9f6a3d96ac8759977ac3d
Moringa School Access <wambuimbeatrice@gmail.com>
2020-09-29 13:04:02 +0300

    Two-way binding

--- src/app/miquotes-form/miquotes-form.component.ts
+++ src/app/miquotes-form/miquotes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Miquotes } from '../miquotes';
 
 @Component({
   selector: 'app-miquotes-form',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiquotesFormComponent implements OnInit {
 
+  newMiquotes = new Miquotes (0,"","", new Date ());
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
